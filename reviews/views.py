777c503fc0e05f667a6bf483b3bbd5d8e0cdda6b
root <root@DESKTOP-A4888S4.localdomain>
2021-04-06 12:18:32 -0400

    untested review form and display

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from reviews.forms import ReviewForm
+from reviews.models import Review
 # Create your views here.
+
+
+def reviews(request, movie):
+    form = ReviewForm()
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Review.objects.create(
+                user=request.user(),
+                movie='movie',
+                text=data['text']
+            )
+    reviews = Review.objects.filter(movie=movie)
+    return render(request, 'reviews.html', {'reviews': reviews, 'form': form})
