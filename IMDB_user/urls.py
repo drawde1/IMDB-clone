ed3af5326c4f5de5ea0e74acb11f9b6a4251747a
April Limas <aprilj.limas@gmail.com>
2021-04-08 04:46:38 -0400

    modified url for removing favorites

--- IMDB_user/urls.py
+++ IMDB_user/urls.py
@@ -4,5 +4,5 @@ from IMDB_user.views import add_favorites, remove_favorites
 
 urlpatterns = [
     path('favorites/<int:movie_id>/', add_favorites, name='add_favorites'),
-    path('remove/<int:movie_id>/', remove_favorites, name='remove_favorites')
+    path('favorites/remove/<int:movie_id>/', remove_favorites, name='remove_favorites')
 ]
